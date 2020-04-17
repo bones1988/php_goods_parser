fad8a52368ccf68ca5d17b75a12896b14bd640e4
bonescccp <bonescccp@gmail.com>
2020-04-17 16:01:47 +0300

    Add first pages without any data in them

---
+++ goods_parser/private/shared/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <?php if(!isset($page_title)) {
+        $page_title = '';
+    } ?>
+    <title><?php echo $page_title; ?></title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+<header>
+    <h1><?php echo $page_title?></h1>
+</header>
---
+++ goods_parser/public/parse_goods.php
@@ -0,0 +1,13 @@
+<?php $page_title = 'File parse'; ?>
+
+<?php
+include_once '../private/shared/header.php';
+require "../private/parser/SimpleXLSX.php";
+?>
+
+<div>
+    <h1>here will be data</h1>
+</div>
+
+<?php include_once '../private/shared/end_of_file.php';?>
+
