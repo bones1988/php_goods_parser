1adff9e94acb5a327f6a5bf15733992fb4e81b3d
bonescccp <bonescccp@gmail.com>
2020-04-17 20:31:15 +0300

    Add composer and phpoffice dependency

--- goods_parser/public/parse_goods.php
+++ goods_parser/public/parse_goods.php
@@ -5,8 +5,14 @@ include_once '../private/shared/header.php';
 require "../private/parser/SimpleXLSX.php";
 ?>
 
-<div>
-    <h1>here will be data</h1>
+<div id="tableData">
+    <?php
+    if($parsed = SimpleXLSX::parse('../private/resources/pricelist.xlsx')) {
+        echo '<h1>Worked</h1>';
+    } else {
+        echo SimpleXLSX::parseError();
+    }
+    ?>
 </div>
 
 <?php include_once '../private/shared/end_of_file.php';?>
